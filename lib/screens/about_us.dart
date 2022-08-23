d2dd264ddee558776e7ec82af518cec9ede276e4
dhruvisakhiya25 <dhr.globaliasoft@gmail.com>
2022-08-23 14:25:00 +0530

    third commit

--- lib/screens/about_us.dart
+++ lib/screens/about_us.dart
@@ -41,12 +41,13 @@ class _AboutUsPageState extends State<AboutUsPage> {
                   itemBuilder: (context, index) => RichText(
                     text: TextSpan(
                         text: '.',
-  //                      style: const TextStyle(color: orange, fontSize: 50),
+                       style: const TextStyle(color: orange, fontSize: 50),
                         children: [
                           TextSpan(
                               text: lorem,
-                              style: const TextStyle(color: black, fontSize: 17)),
-                        ]),
+                              style:
+                      const TextStyle(color: black, fontSize: 17)),
+                        ])
                   ),
                 )
               ],
